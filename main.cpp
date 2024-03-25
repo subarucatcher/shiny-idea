37f03806b76c67e0a42369142acdea62489a2d79
Lambert Bethel <164849069+subarucatcher@users.noreply.github.com>
2024-03-25 09:02:31 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+struct TrieNode
+{
+
+    struct TrieNode *children[26];
+
+    bool isEnd;
+};
+
+struct TrieNode *getNode()
+{
+
+    struct TrieNode *pNode = new TrieNode;
+    pNode->isEnd = false;
+    for (int i = 0; i < 26; i++)
+    {
+
+        pNode->children[i] = NULL;
+    }
+
+    return pNode;
+}
+
+ bool search(string word)
+    {
+        struct TrieNode *pcrawl = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+
+            if (!pcrawl->children[word[i] - 'a'])
+                return false;
